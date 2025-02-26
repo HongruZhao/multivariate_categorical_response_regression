abf22c5fad922841b5348bd4e7468b1b226a4de4
HongruZhao <144969307+HongruZhao@users.noreply.github.com>
2025-02-26 00:59:58 -0600

    Delete read data directory

--- read data/slurm parallel computing/package_local.R
+++
@@ -1,6 +0,0 @@
-install.packages("glmnet",
-                 repos = "https://cran.rstudio.com",
-                 lib = "/panfs/jay/groups/27/shenx/zhao1118/AD-real-data/library")
-install.packages("Matrix",
-                 repos = "https://cloud.r-project.org/",
-                 lib = "/panfs/jay/groups/27/shenx/zhao1118/AD-real-data/library")
